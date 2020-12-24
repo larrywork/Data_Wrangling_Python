44563dc68300e25c407da20471575034855b6492
larrywork <larrywork@users.noreply.github.com>
2020-12-24 00:58:26 -0500

    test1

--- create a calculated column.py
+++ create a calculated column.py
@@ -1,7 +1,8 @@
-print('test')
+import pandas as pd
 
-print('hi')
+df = pd.read_excel('F:/Files/Study/TinyLarry/Projects/Data Wrangling Python/data/raw data.xlsx', engine='openpyxl')
+print(df.head())
 
-print('hi')
-
-print('hi')
+# create 'earlier_date' column: choose the earlier date of 'college_grad_date' and 'wedding_date'
+df['earlier_date'] = df[['grad_date', 'wedding_date']].min(axis=1)
+print(df)
